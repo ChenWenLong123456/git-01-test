6e2db901e225d7b4851dd625e4e23a3ae73ba999
cwl <1603963844@qq.com>
2019-07-30 14:37:44 +0800

    deit at my brach

--- src/main/java/com/bjpowernode/git/HelloWorld.java
+++ src/main/java/com/bjpowernode/git/HelloWorld.java
@@ -3,5 +3,7 @@ package com.bjpowernode.git;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("cccccccccc!!!");
+        System.out.println("dddddddddd!!!");
     }
 }
